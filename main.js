02d6901c3521c84eb07d62d326a0b52c55c9f2a0
toshaqqw <150771297+toshaqqw@users.noreply.github.com>
2025-06-20 18:13:49 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+const openBtn = document.getElementById('openBtn');
+openBtn.addEventListener('click', () => {
+  alert('🎉 Вы открыли подарок!');
+  // Здесь можно вызвать Telegram.WebApp.sendData(...)
+});
